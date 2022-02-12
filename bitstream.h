c00619955e082de4dae151a58f9ab10e741b3aaa
Bartosz S <bartosz.socki@gmail.com>
2022-02-12 22:18:50 +0100

    implementation of bitstream, also error correction

--- bitstream.h
+++ bitstream.h
@@ -1,18 +1,20 @@
-#ifndef _BARTOSZS_BITSTREAM_
-#define _BARTOSZS_BITSTREAM_
+#ifndef _BITSTREAM_IS_INCLUDED_
+#define _BITSTREAM_IS_INCLUDED_
 
 #include <stdint.h>
 
 struct bitstream {
 	uint32_t max_size;
-	uint32_t cur_size;
-	uint8_t* stream;
+	uint32_t cur_pos;
+	uint8_t* bits;
 };
 
-struct bitstream* bitstream_create(uint32_t max_size);
+struct bitstream* bitstream_init(uint32_t max_size);
 struct bitstream* bitstream_deepCopy(struct bitstream* stream);
-void bitstream_write(struct bitstream* stream, uint8_t value, uint8_t size);
-void bitstream_copy(struct bitstream* stream_from, struct bitstream* stream_to);
+void bitstream_copy(struct bitstream* src, struct bitstream* dest);
 void bitstream_free(struct bitstream* stream);
+void bitstream_writeBit(struct bitstream* stream, uint8_t value);
+uint8_t bitstream_removeBit(struct bitstream* stream);
+uint8_t bitstream_getBit(struct bitstream* stream, uint32_t pos);
 
 #endif
